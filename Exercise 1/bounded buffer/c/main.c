94440723312ab2126da6c93e082feeb0d59e6855
Teodor Fossdal <teodor.fossdal@gmail.com>
2024-01-10 10:10:53 +0100

    Finished task 5

--- Exercise 1/bounded buffer/c/main.c
+++ Exercise 1/bounded buffer/c/main.c
@@ -23,8 +23,8 @@ struct BoundedBuffer* buf_new(int size){
     
     pthread_mutex_init(&buf->mtx, NULL);
     // TODO: initialize semaphores
-    //sem_init(&buf->capacity,      0, /*starting value?*/);
-	//sem_init(&buf->numElements,   0, /*starting value?*/);
+    sem_init(&buf->capacity,      0, 4);
+	sem_init(&buf->numElements,   0, 0);
     
     return buf;    
 }
@@ -42,19 +42,28 @@ void buf_destroy(struct BoundedBuffer* buf){
 
 void buf_push(struct BoundedBuffer* buf, int val){    
     // TODO: wait for there to be room in the buffer
+    sem_wait(&buf->capacity);
+
     // TODO: make sure there is no concurrent access to the buffer internals
-    
+    pthread_mutex_lock(&buf->mtx);
     rb_push(buf->buf, val);
+    pthread_mutex_unlock(&buf->mtx);
     
     
     // TODO: signal that there are new elements in the buffer    
+    sem_post(&buf->numElements);
 }
 
 int buf_pop(struct BoundedBuffer* buf){
     // TODO: same, but different?
+    sem_wait(&buf->numElements);
     
+    pthread_mutex_lock(&buf->mtx);
     int val = rb_pop(buf->buf);    
-    
+    pthread_mutex_unlock(&buf->mtx);
+
+    sem_post(&buf->capacity);
+
     return val;
 }
 
--- Exercise 1/bounded buffer/go/main.go
+++ Exercise 1/bounded buffer/go/main.go
@@ -1,38 +1,41 @@
-
 package main
 
-import "fmt"
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
+func producer(ch chan int) {
 
-func producer(/*TODO: parameters?*/){
-
-    for i := 0; i < 10; i++ {
-        time.Sleep(100 * time.Millisecond)
-        fmt.Printf("[producer]: pushing %d\n", i)
-        // TODO: push real value to buffer
-    }
+	for i := 0; i < 10; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Printf("[producer]: pushing %d\n", i)
+		// TODO: push real value to buffer
+		ch <- i
+	}
 
 }
 
-func consumer(/*TODO: parameters?*/){
+func consumer(ch chan int) {
 
-    time.Sleep(1 * time.Second)
-    for {
-        i := 0 //TODO: get real value from buffer
-        fmt.Printf("[consumer]: %d\n", i)
-        time.Sleep(50 * time.Millisecond)
-    }
-    
+	time.Sleep(1 * time.Second)
+	for {
+		select {
+		case i := <-ch: //TODO: get real value from buffer
+			fmt.Printf("[consumer]: %d\n", i)
+			time.Sleep(50 * time.Millisecond)
+		}
+	}
+
 }
 
+func main() {
 
-func main(){
-    
-    // TODO: make a bounded buffer
-    
-    go consumer()
-    go producer()
-    
-    select {}
+	// TODO: make a bounded buffer
+	bufferChan := make(chan int, 4)
+
+	go consumer(bufferChan)
+	go producer(bufferChan)
+
+	select {}
 }
