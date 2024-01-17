d4f38f0fd1704a74e47808748c8257f7e3212fec
JonnyErDigg <yu@exe.com>
2024-01-17 10:40:37 +0100

    Did lots of stuff

--- Exercise 2/client.go
+++ Exercise 2/client.go
@@ -5,29 +5,84 @@ import (
 	"net"
 )
 
+// func getLabAdress() {
+// 	// Create a UDP address to bind to
+// 	addr, err := net.ResolveUDPAddr("udp", ":30000")
+// 	if err != nil {
+// 		fmt.Println("Error resolving UDP address:", err)
+// 		return
+// 	}
+
+// 	fmt.Printf("Adress: %s\n", addr)
+
+// 	// Create a UDP connection
+// 	conn, err := net.ListenUDP("udp", addr)
+// 	if err != nil {
+// 		fmt.Println("Error listening on UDP:", err)
+// 		return
+// 	}
+
+// 	//IP Adress: 10.100.23.129
+
+// 	buf := make([]byte, 1024)
+
+// 	conn.ReadFromUDP(buf)
+
+// 	fmt.Printf("IP Adress: %s\n", buf[:])
+// }
+
+// func main() {
+// 	send_addr, err := net.ResolveUDPAddr("udp", ":20021")
+// 	if err != nil {
+// 		fmt.Println("Error resolving UDP address:", err)
+// 		return
+// 	}
+
+// 	conn_echo, err := net.ListenUDP("udp", send_addr)
+
+// 	if err != nil {
+// 		fmt.Println("Error listening on UDP:", err)
+// 		return
+// 	}
+// 	_, err1 := conn_echo.WriteTo([]byte("Hei på deg"), send_addr)
+// 	if err1 != nil {
+// 		fmt.Println("Error writing to UDP:", err1)
+// 		return
+// 	}
+
+// 	buf_echo := make([]byte, 1024)
+
+// 	conn_echo.ReadFromUDP(buf_echo)
+
+// 	fmt.Printf("Echo: %s\n", buf_echo[:])
+// }
+
 func main() {
-	// Create a UDP address to bind to
-	addr, err := net.ResolveUDPAddr("udp", ":30000")
+	listenAddr, err := net.ResolveUDPAddr("udp4", ":30000")
 	if err != nil {
-		fmt.Println("Error resolving UDP address:", err)
-		return
+		panic(err)
 	}
 
-	fmt.Printf("Adress: %s\n", addr)
-
-	// Create a UDP connection
-	conn, err := net.ListenUDP("udp", addr)
+	conn, err := net.ListenUDP("udp4", listenAddr)
 	if err != nil {
-		fmt.Println("Error listening on UDP:", err)
-		return
+		panic(err)
+	}
+	defer conn.Close()
+
+	addr, err := net.ResolveUDPAddr("udp4", "10.100.23.129:20021")
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = conn.WriteTo([]byte("Halla"), addr)
+	if err != nil {
+		panic(err)
 	}
 
 	buf := make([]byte, 1024)
 
 	conn.ReadFromUDP(buf)
 
-	fmt.Printf("IP Adress: %s", buf[:])
-
-	//defer conn.Close()
+	fmt.Printf("Echo: %s\n", buf[:])
 
 }
