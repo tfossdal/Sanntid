da8f7648dffe547f083cbed65450e3257e24051c
JonnyErDigg <yu@exe.com>
2024-01-17 10:44:43 +0100

    f

---
+++ Exercise 2/reciever_udp.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+ func main() {
+	for {
+ 	// Create a UDP address to bind to
+ 	addr, err := net.ResolveUDPAddr("udp", ":30000")
+ 	if err != nil {
+ 		fmt.Println("Error resolving UDP address:", err)
+ 		return
+ 	}
+ 	fmt.Printf("Adress: %s\n", addr)
+
+
+ 	// Create a UDP connection
+ 	conn, err := net.ListenUDP("udp", addr)
+ 	if err != nil {
+ 		fmt.Println("Error listening on UDP:", err)
+		return
+	}
+
+	//IP Adress: 10.100.23.129
+
+	buf := make([]byte, 1024)
+
+	conn.ReadFromUDP(buf)
+
+	fmt.Printf("IP Adress: %s\n", buf[:])}
