5de96becb51e73ca8500092d906fea3a0e13f5b9
tfossdal <teodor.fossdal@gmail.com>
2024-01-17 10:59:08 +0100

    doesn't work :)

--- Exercise 2/client.go
+++ Exercise 2/client.go
@@ -5,84 +5,28 @@ import (
 	"net"
 )
 
-// func getLabAdress() {
-// 	// Create a UDP address to bind to
-// 	addr, err := net.ResolveUDPAddr("udp", ":30000")
-// 	if err != nil {
-// 		fmt.Println("Error resolving UDP address:", err)
-// 		return
-// 	}
-
-// 	fmt.Printf("Adress: %s\n", addr)
-
-// 	// Create a UDP connection
-// 	conn, err := net.ListenUDP("udp", addr)
-// 	if err != nil {
-// 		fmt.Println("Error listening on UDP:", err)
-// 		return
-// 	}
-
-// 	//IP Adress: 10.100.23.129
-
-// 	buf := make([]byte, 1024)
-
-// 	conn.ReadFromUDP(buf)
-
-// 	fmt.Printf("IP Adress: %s\n", buf[:])
-// }
-
-// func main() {
-// 	send_addr, err := net.ResolveUDPAddr("udp", ":20021")
-// 	if err != nil {
-// 		fmt.Println("Error resolving UDP address:", err)
-// 		return
-// 	}
-
-// 	conn_echo, err := net.ListenUDP("udp", send_addr)
-
-// 	if err != nil {
-// 		fmt.Println("Error listening on UDP:", err)
-// 		return
-// 	}
-// 	_, err1 := conn_echo.WriteTo([]byte("Hei på deg"), send_addr)
-// 	if err1 != nil {
-// 		fmt.Println("Error writing to UDP:", err1)
-// 		return
-// 	}
-
-// 	buf_echo := make([]byte, 1024)
-
-// 	conn_echo.ReadFromUDP(buf_echo)
-
-// 	fmt.Printf("Echo: %s\n", buf_echo[:])
-// }
-
 func main() {
-	listenAddr, err := net.ResolveUDPAddr("udp4", ":30000")
+	send_addr, err := net.ResolveUDPAddr("udp", ":20012")
 	if err != nil {
-		panic(err)
+		fmt.Println("Error resolving UDP address:", err)
+		return
 	}
 
-	conn, err := net.ListenUDP("udp4", listenAddr)
-	if err != nil {
-		panic(err)
-	}
-	defer conn.Close()
+	conn_echo, err := net.ListenUDP("udp", send_addr)
 
-	addr, err := net.ResolveUDPAddr("udp4", "10.100.23.129:20021")
 	if err != nil {
-		panic(err)
+		fmt.Println("Error listening on UDP:", err)
+		return
 	}
-
-	_, err = conn.WriteTo([]byte("Halla"), addr)
+	_, err = conn_echo.WriteTo([]byte("Hei på deg"), send_addr)
 	if err != nil {
-		panic(err)
+		fmt.Println("Error writing to UDP:", err)
+		return
 	}
 
-	buf := make([]byte, 1024)
+	buf_echo := make([]byte, 1024)
 
-	conn.ReadFromUDP(buf)
+	conn_echo.ReadFromUDP(buf_echo)
 
-	fmt.Printf("Echo: %s\n", buf[:])
-
+	fmt.Printf("Echo: %s\n", buf_echo[:])
 }
--- Exercise 2/reciever_udp.go
+++ Exercise 2/reciever_udp.go
@@ -4,28 +4,30 @@ import (
 	"fmt"
 	"net"
 )
- func main() {
+
+func main() {
 	for {
- 	// Create a UDP address to bind to
- 	addr, err := net.ResolveUDPAddr("udp", ":30000")
- 	if err != nil {
- 		fmt.Println("Error resolving UDP address:", err)
- 		return
- 	}
- 	fmt.Printf("Adress: %s\n", addr)
+		// Create a UDP address to bind to
+		addr, err := net.ResolveUDPAddr("udp", ":20012")
+		if err != nil {
+			fmt.Println("Error resolving UDP address:", err)
+			return
+		}
+		fmt.Printf("Adress: %s\n", addr)
 
+		// Create a UDP connection
+		conn, err := net.ListenUDP("udp", addr)
+		if err != nil {
+			fmt.Println("Error listening on UDP:", err)
+			return
+		}
 
- 	// Create a UDP connection
- 	conn, err := net.ListenUDP("udp", addr)
- 	if err != nil {
- 		fmt.Println("Error listening on UDP:", err)
-		return
+		//IP Adress: 10.100.23.129
+
+		buf := make([]byte, 1024)
+
+		conn.ReadFromUDP(buf)
+
+		fmt.Printf("IP Adress: %s\n", buf[:])
 	}
-
-	//IP Adress: 10.100.23.129
-
-	buf := make([]byte, 1024)
-
-	conn.ReadFromUDP(buf)
-
-	fmt.Printf("IP Adress: %s\n", buf[:])}
+}
