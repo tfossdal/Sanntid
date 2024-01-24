a61fe2d8db60c7678341597ecf69c043c87d77b1
Teodor Fossdal <teodor.fossdal@gmail.com>
2024-01-24 13:34:45 +0100

    Ordered stuff

--- Exercise3/driver-go/elevio/requests.go
+++ Exercise3/driver-go/elevio/requests.go
@@ -5,27 +5,6 @@ type DirnBehaviourPair struct {
 	state State
 }
 
-func Requests_ShouldClearImmediately(e Elevator, btn_floor int, btn_type ButtonType) int {
-	switch e.config.clearRequestVariant {
-	case CV_ALL:
-		if e.floor == btn_floor {
-			return 1
-		}
-		return 0
-	case CV_InDirn:
-		if e.floor == btn_floor &&
-			(e.dirn == MD_Up && btn_type == BT_HallUp) ||
-			(e.dirn == MD_Down && btn_type == BT_HallDown) ||
-			e.dirn == MD_Stop ||
-			btn_type == BT_Cab {
-			return 1
-		}
-		return 0
-	default:
-		return 0
-	}
-}
-
 func requests_above(e Elevator) int {
 	for f := e.floor + 1; f < _numFloors; f++ {
 		for btn := 0; btn < _numButtons; btn++ {
@@ -111,5 +90,26 @@ func Requests_shouldStop(e Elevator) int {
 		return 0
 	default:
 		return 1
+	}
+}
+
+func Requests_ShouldClearImmediately(e Elevator, btn_floor int, btn_type ButtonType) int {
+	switch e.config.clearRequestVariant {
+	case CV_ALL:
+		if e.floor == btn_floor {
+			return 1
+		}
+		return 0
+	case CV_InDirn:
+		if e.floor == btn_floor &&
+			(e.dirn == MD_Up && btn_type == BT_HallUp) ||
+			(e.dirn == MD_Down && btn_type == BT_HallDown) ||
+			e.dirn == MD_Stop ||
+			btn_type == BT_Cab {
+			return 1
+		}
+		return 0
+	default:
+		return 0
 	}
 }
