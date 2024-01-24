e3f10b631df091a15cae026506b8e5f367bf93ef
jancal <jancalduchisaksen@gmail.com>
2024-01-24 13:33:17 +0100

    dsadsa

--- Exercise3/driver-go/elevio/fsm.go
+++ Exercise3/driver-go/elevio/fsm.go
@@ -5,8 +5,27 @@ var elevator Elevator = Elevator{-1, MD_Stop, [_numFloors][_numButtons]int{{0, 0
 func Fsm_OnRequestButtonPress(btn_Floor int, btn_type ButtonType) {
 	switch elevator.state {
 	case DoorOpen:
-		if (Requests_ShouldClearImmediately(elevator, btn_Floor, btn_type) != 0){
-			
+		if Requests_ShouldClearImmediately(elevator, btn_Floor, btn_type) != 0 {
+			Timer_start(elevator.config.doorOpenDuration_s)
+		} else {
+			elevator.requests[btn_Floor][btn_type] = 1
+		}
+	case Moving:
+		elevator.requests[btn_Floor][btn_type] = 1
+	case Idle:
+		elevator.requests[btn_Floor][btn_type] = 1
+		var pair DirnBehaviourPair = Requests_chooseDirection(elevator)
+		elevator.dirn = pair.dirn
+		elevator.state = pair.state
+		switch pair.state {
+		case DoorOpen:
+			SetDoorOpenLamp(true)
+			Timer_start(elevator.config.doorOpenDuration_s)
+			//elevator = Request_clearAtCurrentFloor(elevator)
+		case Moving:
+			SetMotorDirection(elevator.dirn)
+		case Idle:
+			break
 		}
 	}
 }
@@ -22,5 +41,4 @@ func Fsm_OnFloorArrival(newFloor int) {
 			return
 		}
 	}
-
 }
